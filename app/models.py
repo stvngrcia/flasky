cb86930b256ac25a02e208379d0520840558dcf8
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-08 09:54:08 -0700

    adding base code and starting security login

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+    pasword_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return ("<user> %r" % self.user_name)
+
+    @property
+    def password(self):
+        raise AttributeError("Password is not a redable attribute")
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
