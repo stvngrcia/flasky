68ab2f1f2d1b4fb0d3627d1bc489eed10b284e39
Steven Garcia <steven.garcia@holbertonschool.com>
2017-08-08 16:26:00 -0700

    Adding log in and log out implementations

--- app/models.py
+++ app/models.py
@@ -1,7 +1,10 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask_login import UserMixin
+from . import login_manager
 
-class Role(db.Model):
+
+class Role(db.Model, UserMixin):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
@@ -10,13 +13,14 @@ class Role(db.Model):
     def __repr__(self):
         return '<Role %r>' % self.name
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     user_name = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
     password_hash = db.Column(db.String(128))
+    email = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return ("<user> %r" % self.user_name)
@@ -31,3 +35,8 @@ class User(db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return(User.query.get(int(user_id)))
